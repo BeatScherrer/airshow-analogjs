89b4b9926637052d7b017ed77c35aa0abb3729d7
BeatScherrer <beat.scherrer@gmail.com>
2025-03-06 20:40:16 +0100

    add footer

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from "@angular/core";
 import { ButtonModule } from "primeng/button";
 import { NavigationComponent } from "@components/navigation/navigation.component";
+import { FooterComponent } from "@components/footer/footer.component";
 
 @Component({
   selector: "app-home",
   standalone: true,
-  imports: [ButtonModule, NavigationComponent],
+  imports: [ButtonModule, NavigationComponent, FooterComponent],
   templateUrl: "./index.page.html",
 })
 export default class HomeComponent {
