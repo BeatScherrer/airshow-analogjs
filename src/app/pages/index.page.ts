205888697c2652a82ea6f5050dbfefb56e10e72c
BeatScherrer <beat.scherrer@gmail.com>
2025-03-09 21:04:32 +0100

    cleanup index page

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -21,10 +21,4 @@ import { HeroComponent } from "@components/hero/hero.component";
   ],
   templateUrl: "./index.page.html",
 })
-export default class HomeComponent {
-  count = signal(0);
-
-  increment() {
-    this.count.update((count) => count + 1);
-  }
-}
+export default class HomeComponent {}
