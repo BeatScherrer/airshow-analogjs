c405aa13f40b3f084ed9328b81b6ad548b0106bf
Beat Scherrer <b.scherrer@mt-robot.com>
2025-03-10 16:55:54 +0100

    update hero

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,5 +1,8 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { toSignal } from "@angular/core/rxjs-interop";
+import { AppStateService, ThemeMode } from "@services/app-state.service";
 import { ButtonModule } from "primeng/button";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: "app-hero",
@@ -7,4 +10,12 @@ import { ButtonModule } from "primeng/button";
   templateUrl: "./hero.component.html",
   styleUrl: "./hero.component.css",
 })
-export class HeroComponent {}
+export class HeroComponent {
+  appStateService = inject(AppStateService);
+
+  isLightMode = toSignal(
+    this.appStateService
+      .getThemeMode()
+      .pipe(map((mode) => mode === ThemeMode.LIGHT)),
+  );
+}
