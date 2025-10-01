64700f5669846279bc11e3e4a76db43608cdbb74
Beat Scherrer <beat.scherrer@gmail.com>
2025-10-01 12:07:44 +0200

    add vercel analytics

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,10 +1,13 @@
 import "zone.js/node";
 import "@angular/platform-server/init";
 import { render } from "@analogjs/router/server";
-import { injectSpeedInsights } from "@vercel/speed-insights";
 import { AppComponent } from "./app/app.component";
 import { config } from "./app/app.config.server";
 
+import { injectSpeedInsights } from "@vercel/speed-insights";
+import { inject } from "@vercel/analytics";
+
 injectSpeedInsights();
+inject();
 
 export default render(AppComponent, config);
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import "zone.js";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
 import { appConfig } from "./app/app.config";
+
 import { injectSpeedInsights } from "@vercel/speed-insights";
+import { inject } from "@vercel/analytics";
 
 injectSpeedInsights();
+inject();
 
 bootstrapApplication(AppComponent, appConfig);
