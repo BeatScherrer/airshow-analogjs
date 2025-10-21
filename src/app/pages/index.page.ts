613d1d18c8b366a2a807be9c55d146830cc43647
Beat Scherrer <beat.scherrer@gmail.com>
2025-10-21 23:13:57 +0200

    remove primeng

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -1,18 +1,12 @@
 import { Component } from "@angular/core";
 import { NavigationComponent } from "@components/navigation/navigation.component";
 import { FooterComponent } from "@components/footer/footer.component";
-import { ContactComponent } from "@components/contact/contact.component";
 import { ProgramComponent } from "@components/program/program.component";
 
 @Component({
   selector: "app-home",
   standalone: true,
-  imports: [
-    NavigationComponent,
-    FooterComponent,
-    ContactComponent,
-    ProgramComponent,
-  ],
+  imports: [NavigationComponent, FooterComponent, ProgramComponent],
   templateUrl: "./index.page.html",
 })
 export default class HomeComponent {}
